70ea05d3b945935ae61f06a48a0579c86dce3c87
ClemBesacier <clementinebesacier@yahoo.fr>
2020-06-25 11:18:11 +0200

    onepage

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+function juizScrollTo(element) {
+    $(element).click(function () {
+        var goscroll = false;
+        var the_hash = $(this).attr("href");
+        var regex = new RegExp("\#(.*)", "gi");
+        var the_element = '';
+
+        if (the_hash.match("\#(.+)")) {
+            the_hash = the_hash.replace(regex, "$1");
+
+            if ($("#" + the_hash).length > 0) {
+                the_element = "#" + the_hash;
+                goscroll = true;
+            }
+            else if ($("a[name=" + the_hash + "]").length > 0) {
+                the_element = "a[name=" + the_hash + "]";
+                goscroll = true;
+            }
+
+            if (goscroll) {
+                $('html, body').animate({
+                    scrollTop: $(the_element).offset().top
+                }, 'slow');
+                return false;
+            }
+        }
+    });
+};
+juizScrollTo('a[href^="#"]');
+
 $(document).ready(function () {
     $('.menu li').click(function () {
         $('.linkMenu').removeClass('active');
@@ -7,4 +37,9 @@ $(document).ready(function () {
     $('.save').click(function () {
         $(this).toggleClass('active');
     })
+
+    $('.account').click(function () {
+        $('.myAccount').toggleClass('active');
+    })
 });
+
